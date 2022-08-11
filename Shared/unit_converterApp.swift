2f51d87ad53ed96fdf0243e4567d130b18610726
Tomi Inouye <tinouye14@gmail.com>
2022-08-10 22:17:28 -0700

    Initial Commit

---
+++ Shared/unit_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  unit_converterApp.swift
+//  Shared
+//
+//  Created by Media Room on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct unit_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
