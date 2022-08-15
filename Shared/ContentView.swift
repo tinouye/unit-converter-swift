adbe232e2183fff8c4b1505b145236913ae2c482
Tomi Inouye <tinouye14@gmail.com>
2022-08-14 20:13:18 -0700

    Initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var testInput: String = "20"
+    @State private var testResult: String = "asdf"
+    var testAr: [String] = ["Apple", "Banana", "Cherry", "Durian"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            TextField("test", text: $testInput)
+            Text("Hello \(testInput)")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (2nd generation)"))
     }
 }
