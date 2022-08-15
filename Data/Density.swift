701f5026f6ca06267d193b8300d5cb1fc0742e11
Tomi Inouye <tinouye14@gmail.com>
2022-08-14 22:16:18 -0700

    Rudimentary structure with placeholders

---
+++ Data/Density.swift
@@ -0,0 +1,13 @@
+//
+//  Density.swift
+//  unit-converter (iOS)
+//
+//  Created by Media Room on 8/14/22.
+//
+
+import Foundation
+
+struct Density: Hashable, Codable {
+    var ingredient: String
+    var density: Float32
+}
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,11 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @State private var testInput: String = "20"
     @State private var testResult: String = "asdf"
+    @State private var currIngredient: Int = 0
     var testAr: [String] = ["Apple", "Banana", "Cherry", "Durian"]
+    
+    func UpdateCurrentIngredient(index:Int) {
+        currIngredient = index
+    }
+    
     var body: some View {
         VStack{
             TextField("test", text: $testInput)
-            Text("Hello \(testInput)")
+            Text("Hello \(testInput) \(testResult)")
+            Menu("Test Menu") {
+                ForEach(testAr.indices, id: \.self) {index in
+                    Button(testAr[index], action: {
+                        currIngredient = index
+                        testResult = testAr[index]
+                    })
+                }
+            }
         }
     }
 }
