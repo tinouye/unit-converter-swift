e92612097949779fba8d32eb1612318477512804
Tomi Inouye <tinouye14@gmail.com>
2022-08-17 23:00:20 -0700

    Added guard statements to conversion units

--- Shared/Conversions.swift
+++ Shared/Conversions.swift
@@ -8,15 +8,37 @@
 import Foundation
 import SwiftUI
 
-func updateDensityCalc(val valStr: String, unit unitStr: String, density densityStr: String) -> String {
-
+func updateDensityCalc(val valStr: String, inputUnit inputUnitStr: String, outputUnit outputUnitStr: String, density densityStr: String) -> String {
+    
+    let unitsDict = ["g": UnitMass.grams,
+        "kg": UnitMass.kilograms,
+        "ml": UnitVolume.milliliters,
+        "tsp": UnitVolume.teaspoons,
+        "tbsp": UnitVolume.tablespoons,
+        "cup": UnitVolume.cups
+    ]
+    
     var outputMeasurement: Double = 0.0
     guard let val = Double(valStr) else {
-        return String(outputMeasurement)
+        return "Invalid Value"
     }
     guard let density = Double(densityStr) else {
-        return String(outputMeasurement+1)
+        return "Error with density value"
     }
+    guard let inputUnit = unitsDict[inputUnitStr] else {
+        //Error
+        return "Invalid Input Unit"
+    }
+    guard let outputUnit = unitsDict[outputUnitStr] else {
+        return "Invalid Output Unit"
+    }
+    
+    let inputMeasurement: Measurement = Measurement(value:val, unit: inputUnit)
+    
+    if inputMeasurement.unit is UnitMass && outputUnit is UnitMass {
+        print("foo")
+    }
+    
     
     //let inputMeasurement = Measurement(value: val, unit: UnitMass.grams)
     outputMeasurement = val/density
